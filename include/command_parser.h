bb70fe391674614f845d1292662030269a7e2d40
1024th <871712476@qq.com>
2022-05-03 20:45:04 +0800

    Add header

---
+++ include/command_parser.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+/**
+ * @brief 解析输入指令。
+ *
+ */
+class CommandParser {
+ public:
+  /**
+   * @brief 循环读入指令并解析，直到遇到 exit。
+   *
+   */
+  void Run();
+  /**
+   * 这是 Add 的注释
+   * @param x 这是对参数 x 的注释
+   * @return 这是对返回值的注释
+   */
+  int Add(int x);
+};
